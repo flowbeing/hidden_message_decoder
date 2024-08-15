7ea49d6b8ddc934f051452e7ada6466a377d757d
admin <flowbeing@proton.me>
2024-08-15 23:43:47 +0100

    filtered countries data to show only countries named Zambia

--- countries_and_their_codes.py
+++ countries_and_their_codes.py
@@ -10,5 +10,6 @@ table_one=tables[0]
 table_one['Numeric']=[int(i) for i in table_one['Numeric'].to_list()]
 
 table_one.sort_values('Numeric', ascending=True, inplace=True)
+print(table_one.loc[table_one['Country']=='Zambia'])
 
-print(table_one)
+# print(table_one)
